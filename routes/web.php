0facafd67f8a53ace327691b024116a088df5085
ishado52 <ishado52@gmail.com>
2024-06-20 06:00:18 +0900

    feat:投稿一覧ページ作成

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::resource('/', PostController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
